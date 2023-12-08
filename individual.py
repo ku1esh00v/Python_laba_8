66232cbe2f739f8807ac7578ee603ab59d0d3812
ku1esh00v <oleg.kuleshov.1261@mai.ru>
2023-12-09 02:51:21 +0300

    индивидуальное задание

---
+++ individual.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+if __name__ == "__main__":
+    my_tuple = tuple(map(int, input("Enter the elements of the tuple separated by a space: ").split()))
+
+    last_even_index = -1
+
+    for i in range(len(my_tuple) - 1):
+        if my_tuple[i] % 2 == 0 and my_tuple[i + 1] % 2 == 0:
+            last_even_index = i
+
+    if last_even_index != -1:
+        temp_result = my_tuple[:last_even_index]
+        result = (' '.join(map(str, temp_result)))
+
+        if result == '':
+            print('There are no elements before the last pair of adjacent even numbers.')
+        else:
+            print(result)
+    else:
+        print("There are no pairs of adjacent even numbers in the tuple!", file=sys.stderr)
